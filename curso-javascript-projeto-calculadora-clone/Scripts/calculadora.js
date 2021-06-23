c80e11e254fb88e0856413bbfe0d5bc1b47e0af4
JoaoPedro1417 <86246599+JoaoPedro1417@users.noreply.github.com>
2021-06-23 17:25:12 -0300

    Add files via upload

---
+++ curso-javascript-projeto-calculadora-clone/Scripts/calculadora.js
@@ -0,0 +1,6 @@
+/*O objeto representa uma classe, facilita o uso do codigo em outros proejetos apena com o uso do objeto (Classe)
+esse uso é chamado de instância*/ 
+
+let calculadora = new calcControler; 
+
+calculadora.displayCalc; 
