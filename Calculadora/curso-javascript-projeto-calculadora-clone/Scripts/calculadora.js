d0201baa5010cfb9ae7419a0ff57c1169d5cbf89
JoaoPedro1417 <86246599+JoaoPedro1417@users.noreply.github.com>
2021-06-29 14:56:10 -0300

    Delete Calculadora/curso-javascript-projeto-calculadora-clone directory

--- Calculadora/curso-javascript-projeto-calculadora-clone/Scripts/calculadora.js
+++
@@ -1,5 +0,0 @@
-/*O objeto representa uma classe, facilita o uso do codigo em outros proejetos apena com o uso do objeto (Classe)
-esse uso é chamado de instância*/ 
-
-window.calculadora = new CalcControler(); 
-
